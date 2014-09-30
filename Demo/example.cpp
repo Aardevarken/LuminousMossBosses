9698d7e1f3be75edb481f6865866cc95f482664b
Andrey Shprengel <andrey.shprengel@colorado.edu>
2014-09-29 18:49:24 -0600

    Added a Cmakelist.txt

--- Demo/example.cpp
+++ Demo/example.cpp
@@ -4,6 +4,7 @@
 	#include <imgproc.h>
 #else
 	#include <opencv/cv.h>
+	#include <opencv/highgui.h>
 	#include <opencv2/core/core.hpp>
 	#include <opencv2/highgui/highgui.hpp>
 	#include <opencv2/imgproc/imgproc.hpp>
