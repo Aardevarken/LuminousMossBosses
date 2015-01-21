9798f7839a44a4423332610207b353799cb385e4
Aardevarken <morgan.garske@colorado.edu>
2015-01-21 05:10:32 +0000

    added method to convert the result from an sqlalchemy query to valid json. API is now returning valid json for /test_records.

--- gpstest_API/gpstest_API.py
+++ gpstest_API/gpstest_API.py
@@ -15,28 +15,36 @@ def query_database(query):
     query_result = gps_conn.execute(query)
     return query_result 
 
+def result_to_json(query_result):
+    column_names = query_result.keys()
+    row_list = []
+    for row in query_result:
+        i = 0
+        row_dict = {}
+        for item in row:
+            #the split is used to do away with the 'u' that 
+            #sqlalchemy puts in front of every key name
+            row_dict[str(column_names[i]).split('\'')[0]] = str(item)
+            i += 1
+        row_list.append(row_dict)
+    #replace single quote with double for valid json
+    json_result = str(row_list).replace('\'', '"')
+    return json_result
+
 @app.route('/')
 def api_root():
     return 'Welcome\n'
 
 @app.route('/test_records')
 def api_test_records():
-#    result = query_database('SELECT * FROM test_data WHERE model_number = \'test\'')
-#    eng = create_engine('mysql://%s:%s@%s/%s' % (USER, PASSWORD, HOST, DATABASE), echo = True)
-#    conn = eng.connect()
-#    result = conn.execute('SELECT * FROM test_data WHERE model_number = \'test\'')
     result = query_database('SELECT * FROM test_data WHERE model_number = \'test\'')
-    items = []
-    for row in result:
-        items.append(row)
-        print items
-    return str(items)
+    #be aware that this call will close the result object, and it will not be useable afterward.
+    items = result_to_json(result)
+    return items 
 
 @app.route('/articles/<articleid>')
 def api_article(articleid):
     return 'You are reading ' + articleid + '\n'
 
 if __name__ == '__main__':
-    #this is just attempting to establish a connection and get info from the database. Move this into the
-    #API calls later.
     app.run(host='0.0.0.0')
