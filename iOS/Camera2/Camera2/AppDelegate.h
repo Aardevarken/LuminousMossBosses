cadd71d32b83648cc96ce80c29e8aff024e0a07b
BillytheMagicToilet <jack.skinner@colorado.edu>
2015-01-27 08:00:34 -0700

    Added basic Camera App

---
+++ iOS/Camera2/Camera2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Camera2
+//
+//  Created by Jack Skinner on 1/20/15.
+//  Copyright (c) 2015 LuminousMossBoss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS/Camera2/Camera2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Camera2
+//
+//  Created by Jack Skinner on 1/20/15.
+//  Copyright (c) 2015 LuminousMossBoss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
