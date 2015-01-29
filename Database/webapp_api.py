cd1aaeded8f840fba6908a1f1aa1fba1b563e0f5
Jamie Miller <jamie.miller273@gmail.com>
2015-01-29 02:25:29 -0700

    Adding support for finding flower detections.

--- Database/webapp_api.py
+++ Database/webapp_api.py
@@ -30,7 +30,6 @@ returns all observations, most recent first
 """
 @app.route('/observations', methods=['GET'])
 def get_observations():
-    #app.make_default_options_response().headers['Access-Control-Allow-Origin'] = "*"
     try:
         result = query_database(text('SELECT ObsID, images.ImageID, FileName FROM observations LEFT JOIN images ON observations.ImageID=images.ImageID ORDER BY Date DESC, Time DESC;'))
     except:
@@ -39,6 +38,15 @@ def get_observations():
     items = result_to_json(result)
     return items 
 
+@app.route('/flowerdetections/<imageid>', methods=['GET'])
+def get_flowerdetections(imageid):
+    try:
+        result = query_database(text('SELECT * from objlocations WHERE ImageID=' + str(imageid) + ';'))
+    except:
+        return "Error occured. Database may be down. Try again later and check your parameters.\n"
+    #be aware that this call will close the result object, and it will not be useable afterward.
+    items = result_to_json(result)
+    return items 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
