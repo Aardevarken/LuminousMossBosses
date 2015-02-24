a6112d74bca2a97b567f65f1d8d9c25756f0b7b2
BillytheMagicToilet <jack.skinner@colorado.edu>
2015-02-23 21:35:49 -0700

    Added working camera app

---
+++ iOS/CameraApp/CameraApp/APPAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  APPAppDelegate.h
+//  CameraApp
+//
+//  
+//
+
+#import <UIKit/UIKit.h>
+
+@class APPViewController;
+
+@interface APPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) APPViewController *viewController;
+
+@end
