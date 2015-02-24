d386bcc29fdedc318da8c81f8cd916330e5837ac
Morgan Garske <morgan.garske@colorado.edu>
2015-02-24 15:32:37 -0700

    converted Identified. Replaced == overload with isEqual method (no op overloading in Java :( )

--- Android/app/src/main/java/com/luminousmossboss/luminous/Identified.java
+++ Android/app/src/main/java/com/luminousmossboss/luminous/Identified.java
@@ -50,6 +50,126 @@ public class Identified {
         select = false;
     }
 
+    public Identified(double x, double y, double r, double glx, double gly, double glr, boolean state, boolean fp)
+    {
+        this.x = (int)x;
+        this.y = (int)y;
+        this.r = (int)r;
+
+        this.glx = glx;
+        this.gly = gly;
+        this.glr = glr;
+
+        OpenCV = state;
+        falsePositive = fp;
+        select = false;
+    }
+
+    /* put copy constructor here if necessary */
+
+
+    /*
+     * gettters
+     */
+
+    public double getGlx()
+    {
+        return glx;
+    }
+
+    public double getGly()
+    {
+        return gly;
+    }
+
+    public double getGlr()
+    {
+        return glr;
+    }
+
+    public int getCVx()
+    {
+        return x;
+    }
+
+    public int getCVy()
+    {
+        return y;
+    }
+
+    public int getCVr()
+    {
+        return r;
+    }
+
+    public boolean byOpenCV()
+    {
+        return OpenCV;
+    }
+
+    public void setGl(double x, double y, double r, int width, int height, double asp_src)
+    {
+        glx = x;
+        gly = y;
+        glr = r;
+        this.x = convertToCVx(glx, width, asp_src);
+        this.y = convertToCVy(y, height);
+        this.r = convertToCVr(r, width, height, asp_src);
+    }
+
+    public void setGlx(double n, int width, double asp_src)
+    {
+        glx = n;
+        this.x = convertToCVx(glx, width, asp_src);
+    }
+
+    public void setGly(double n, int height, double asp_src)
+    {
+        gly = n;
+        this.y = convertToCVy(gly, height);
+    }
+
+    public void setGlr(double n, int width, int height, double asp_src)
+    {
+        glr = n;
+        this.r = convertToCVr(glr,width, height, asp_src);
+    }
+
+    public void setCV(double x, double y, double r, int width, int height, double asp_src)
+    {
+        this.x = (int)x;
+        this.y = (int)y;
+        this.r = (int)r;
+        glx = convertToGLx(this.x, width, asp_src);
+        gly = convertToGLy(this.y, height);
+        glr = convertToGLr(this.r, width, height, asp_src);
+    }
+
+    public void setCVx(double n, int width, double asp_src)
+    {
+        x = (int)n;
+        glx = convertToGLx(x, width, asp_src);
+    }
+
+    public void setCVy(double n, int height, double asp_src)
+    {
+        y = (int)n;
+        gly = convertToGLy(y, height);
+    }
+
+    public void setCVr(double n, int width, int height, double asp_src)
+    {
+        r = (int)n;
+        glr = convertToGLr(r, width, height, asp_src);
+    }
+
+    public boolean isEqual(Identified id)
+    {
+        return (x == id.getCVx() && y == id.getCVy() && r == id.getCVr() &&
+                glx == id.getGlx() && gly == id.getGly() && glr == id.getGlr() &&
+                OpenCV == id.byOpenCV() && falsePositive == id.falsePositive);
+    }
+
     /*
      * Convert gl coordinates to cv
      */
