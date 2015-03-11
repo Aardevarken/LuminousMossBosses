44225547189a1d986a17b4b1e133ed7c79a80871
Andrey Shprengel <andrey.shprengel@colorado.edu>
2015-03-11 01:00:57 -0600

    Working sending observationsv2

---
+++ Android/app/src/main/java/com/luminousmossboss/luminous/model/Observation.java
@@ -0,0 +1,59 @@
+package com.luminousmossboss.luminous.model;
+
+import android.net.Uri;
+
+import java.io.Serializable;
+
+/**
+ * Created by Andrey on 2/1/2015.
+ */
+
+public class Observation extends ListItem implements Serializable {
+    private String date;
+    private int status;
+    final private int MAXCHAR = 10;
+
+
+    private double longitude;
+    private double latitude;
+    private boolean isStatusVisible = false;
+
+    public Observation() {}
+
+    public Observation(String title, Uri icon) {
+        super(title, icon);
+    }
+
+    public Observation(String title, Uri icon, String date , double latitude, double longitude) {
+        super(title, icon);
+        this.date = date;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getDate() { return shortenString(date,MAXCHAR);}
+
+    public String getFullData() { return this.date;}
+
+    public void setDate(String date){
+        this.date = date;
+    }
+
+    public void setStatus(int status) {this.status = status; }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+}
