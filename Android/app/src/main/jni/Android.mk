6b442a53af7d4728bbb09304279b4943bd123499
Morgan Garske <morgan.garske@colorado.edu>
2015-03-17 21:28:55 -0600

    changed slashes

--- Android/app/src/main/jni/Android.mk
+++ Android/app/src/main/jni/Android.mk
@@ -4,11 +4,11 @@ include $(CLEAR_VARS)
 
 #opencv
 #change this to your Android OpenCV root folder
-OPENCVROOT := C:\ocvAndroid
+OPENCVROOT := ~/Downloads/OpenCV-2.4.10-android-sdk
 OPENCV_INSTALL_MODULES := on
 OPENCV_CAMERA_MODULES := off
 OPENCV_LIB_TYPE := STATIC
-include ${OPENCVROOT}\sdk\native\jni\OpenCV.mk
+include ${OPENCVROOT}/sdk/native/jni/OpenCV.mk
 
 LOCAL_SRC_FILES := detector.cpp   \
                    identified.cpp \
