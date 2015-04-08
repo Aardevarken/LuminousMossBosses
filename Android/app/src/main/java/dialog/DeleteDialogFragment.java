0d57b9f0d999a8cb706b61faf5749921ae51a447
Lambent Fox <lambentfox@github.com>
2015-04-08 11:35:28 -0600

    fixing issues with lower/upper case

--- Android/app/src/main/java/dialog/DeleteDialogFragment.java
+++ Android/app/src/main/java/dialog/DeleteDialogFragment.java
@@ -1,14 +1,17 @@
 package dialog;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.luminousmossboss.luminous.DbHandler;
 import com.luminousmossboss.luminous.MainActivity;
+import com.luminousmossboss.luminous.ObservationFragment;
 import com.luminousmossboss.luminous.R;
 import com.luminousmossboss.luminous.model.Observation;
 
@@ -17,28 +20,35 @@ import com.luminousmossboss.luminous.model.Observation;
  */
 public class DeleteDialogFragment extends DialogFragment{
 
-    private boolean confirmation;
+    private DialogListener mListener;
+
+    public static DeleteDialogFragment getInstance(DialogListener dialogListener) {
+        DeleteDialogFragment deleteDialogFragment = new DeleteDialogFragment();
+
+        Bundle args = new Bundle();
+        args.putSerializable("dialogListener", dialogListener);
+        return deleteDialogFragment;
+
+    }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        mListener = (DialogListener) getArguments().getSerializable("dialogListener");
+
         builder.setMessage(R.string.dialog_remove_obs_msg)
                 .setPositiveButton(R.string.dialog_confirm, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
-                        confirmation = true;
+                        mListener.onDialogPositiveClick(DeleteDialogFragment.this);
                     }
                 })
                 .setNegativeButton(R.string.dialog_decline, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
-                        confirmation = false;
                     }
-        });
+                });
         return builder.create();
-    }
-
-    public boolean getConfirmation() {
-        return confirmation;
     }
 }
---
+++ Android/app/src/main/java/dialog/DialogListener.java
@@ -0,0 +1,7 @@
+package dialog;
+
+/**
+ * Created by Brian on 4/8/2015.
+ */
+public class DialogListener {
+}
