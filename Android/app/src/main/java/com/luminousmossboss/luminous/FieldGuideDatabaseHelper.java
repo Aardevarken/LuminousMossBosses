ce699af4ddce1468c23d93586a589b2088ff736f
aardevarken <morgan.garske@colorado.edu>
2015-04-09 16:37:27 -0600

    field guide database, database helper, and glossary images

---
+++ Android/app/src/main/java/com/luminousmossboss/luminous/FieldGuideDatabaseHelper.java
@@ -0,0 +1,18 @@
+package com.luminousmossboss.luminous;
+
+import android.content.Context;
+
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+/**
+ * Created by MGarske on 4/9/2015.
+ */
+public class FieldGuideDatabaseHelper extends SQLiteAssetHelper {
+
+    private static final String DATABASE_NAME = "FieldGuide.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public FieldGuideDatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+}
