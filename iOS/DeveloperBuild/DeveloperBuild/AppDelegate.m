0dfd0d660556df7031c671d83fa146a4b1fbaff8
Jacob Rail <jacot.rail@colorado.edu>
2015-04-12 19:56:33 -0600

    commited changes to Info.plist and AppDelegate.m

--- iOS/DeveloperBuild/DeveloperBuild/AppDelegate.m
+++ iOS/DeveloperBuild/DeveloperBuild/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "UserDataDatabase.h"
 
 @interface AppDelegate ()
 
@@ -18,6 +19,18 @@ NSMutableArray *_observations;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
+	if (![CLLocationManager locationServicesEnabled]) {
+		// location services is disabled, alert user
+		UIAlertView *servicesDisabledAlert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"DisabledTitle", @"DisabledTitle")
+																		message:NSLocalizedString(@"DisabledMessage", @"DisabledMessage")
+																	   delegate:nil
+															  cancelButtonTitle:NSLocalizedString(@"OKButtonTitle", @"OKButtonTitle")
+															  otherButtonTitles:nil];
+		[servicesDisabledAlert show];
+	}
+	else{
+		[[UserDataDatabase getSharedInstance] startLocationTracking];
+	}
 	return YES;
 }
 
