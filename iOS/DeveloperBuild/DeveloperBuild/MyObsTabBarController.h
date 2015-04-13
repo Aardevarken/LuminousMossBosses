b9d718d0dfaf6c996a6edecb3668f774132f45ea
Jacob Rail <jacot.rail@colorado.edu>
2015-04-13 17:25:52 -0600

    added camera bar icon when in my observations

---
+++ iOS/DeveloperBuild/DeveloperBuild/MyObsTabBarController.h
@@ -0,0 +1,14 @@
+//
+//  MyObsTabBarController.h
+//  DeveloperBuild
+//
+//  Created by Jacob Rail on 4/13/15.
+//  Copyright (c) 2015 CU Boulder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyObsTabBarController : UITabBarController
+@property (strong, nonatomic) IBOutlet UIBarButtonItem *camerBarBarIcon;
+
+@end
---
+++ iOS/DeveloperBuild/DeveloperBuild/MyObsTabBarController.m
@@ -0,0 +1,47 @@
+//
+//  MyObsTabBarController.m
+//  DeveloperBuild
+//
+//  Created by Jacob Rail on 4/13/15.
+//  Copyright (c) 2015 CU Boulder. All rights reserved.
+//
+
+#import "MyObsTabBarController.h"
+#import "ImageViewController.h"
+
+
+@interface MyObsTabBarController ()
+
+@end
+
+@implementation MyObsTabBarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+#pragma mark - Navigation
+/*
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+
+	if ([segue.identifier isEqualToString:@"CameraBarButtonSegue"]) {
+		
+		// create instance of destination
+		ImageViewController *destViewController = segue.destinationViewController;
+		
+		[[self.navigationController] ]
+		
+	}
+}
+*/
+@end
