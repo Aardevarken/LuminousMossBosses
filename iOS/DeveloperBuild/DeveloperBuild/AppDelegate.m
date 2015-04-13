eb688a81dbf98db2131225563b7ead785061e978
Jacob Rail <jacot.rail@colorado.edu>
2015-04-12 22:17:49 -0600

    GPS stops updating when user leaves app, the GPS icon may remain for a few seconds, but the GPS is not fuctioning

--- iOS/DeveloperBuild/DeveloperBuild/AppDelegate.m
+++ iOS/DeveloperBuild/DeveloperBuild/AppDelegate.m
@@ -34,6 +34,7 @@ NSMutableArray *_observations;
 	return YES;
 }
 
+
 - (void)applicationWillResignActive:(UIApplication *)application {
 	// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
 	// Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
@@ -42,10 +43,13 @@ NSMutableArray *_observations;
 - (void)applicationDidEnterBackground:(UIApplication *)application {
 	// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
 	// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+	NSLog(@"Application Did Enter Background");
+	[[UserDataDatabase getSharedInstance] stopLocationTracking];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
 	// Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+	NSLog(@"Application Will Enter Foreground");
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
