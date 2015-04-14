5b94c9f52b23019cdcb67966a9d6755aa826ab23
Jacob Rail <jacot.rail@colorado.edu>
2015-04-13 23:22:46 -0600

    cleaned up commented code. No documentation. Needs code review.

--- iOS/DeveloperBuild/DeveloperBuild/NewObs.h
+++ iOS/DeveloperBuild/DeveloperBuild/NewObs.h
@@ -6,6 +6,10 @@
 //  Copyright (c) 2015 CU Boulder. All rights reserved.
 //
 
+/**
+ * NOT SURE WHERE THIS INTERFACE IS USED
+ */
+
 #import <Foundation/Foundation.h>
 
 @interface NewObs : NSObject
@@ -20,37 +24,3 @@
 			   andObsID: (NSNumber*) newObsID;
 
 @end
-
-
-/*
- #import <Foundation/Foundation.h>
- 
- @interface City : NSObject
- 
- @property (nonatomic, strong) NSString * cityName;
- @property (nonatomic, strong) NSString * cityID;
- @property (nonatomic, strong) NSString * cityState;
- @property (nonatomic, strong) NSString * cityPopulation;
- @property (nonatomic, strong) NSString * cityCountry;
- 
- 
- @property (nonatomic, strong) NSString * fileName;
- @property (nonatomic, strong) NSNumber * imageID;
- @property (nonatomic, strong) NSString * fileLocation;
- 
- 
- 
- //Methods
- - (id) initWithCityID: (NSString *) cID andCityName: (NSString *) cName andCityState: (NSString *) cState andCityPopulation: (NSString *) cPopulation andCityCountry: (NSString *) cCountry;
- 
- @end
- 
- */
-
-/*
- 2015-02-13 17:53:49.581 JSONiOS[2993:121605] #0: {
- FileName = "Silene-acaulis-copy.jpg";
- ImageID = 211;
- ObsID = 215;
- }
- */
