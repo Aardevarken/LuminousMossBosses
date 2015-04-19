65024fbe1273a39f3d58b616d352039b2c95ed2c
Jacob Rail <jacot.rail@colorado.edu>
2015-04-19 16:29:29 -0600

    prep for first attempt to implement custom camera view into ImageViewControler.m

--- iOS/DeveloperBuild/DeveloperBuild/ImageViewController.m
+++ iOS/DeveloperBuild/DeveloperBuild/ImageViewController.m
@@ -11,7 +11,7 @@
 #import "MyObservations.h"
 #import "UserDataDatabase.h"
 #import <AssetsLibrary/AssetsLibrary.h>
-
+#import <AVFoundation/AVFoundation.h>
 
 #define TESTING YES
 
@@ -199,6 +199,47 @@
 
 }
  */
+
+/* Test code here */
+- (void)initCapture
+{
+	AVCaptureDevice *inputDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+	AVCaptureDeviceInput *captureInput = [AVCaptureDeviceInput deviceInputWithDevice:inputDevice error:nil];
+	if (!captureInput) {
+		return;
+	}
+	AVCaptureVideoDataOutput *captureOutput = [[AVCaptureVideoDataOutput alloc] init];
+	/* captureOutput:didOutputSampleBuffer:fromConnection delegate method !*/
+	[captureOutput setSampleBufferDelegate:self queue:dispatch_get_main_queue()];
+	NSString* key = (NSString*)kCVPixelBufferPixelFormatTypeKey;
+	NSNumber* value = [NSNumber numberWithUnsignedInt:kCVPixelFormatType_32BGRA];
+	NSDictionary* videoSettings = [NSDictionary dictionaryWithObject:value forKey:key];
+	[captureOutput setVideoSettings:videoSettings];
+	self.captureSession = [[AVCaptureSession alloc] init];
+	NSString* preset = 0;
+	if (!preset) {
+		preset = AVCaptureSessionPresetMedium;
+	}
+	self.captureSession.sessionPreset = preset;
+	if ([self.captureSession canAddInput:captureInput]) {
+		[self.captureSession addInput:captureInput];
+	}
+	if ([self.captureSession canAddOutput:captureOutput]) {
+		[self.captureSession addOutput:captureOutput];
+	}
+	
+	//handle prevLayer
+	if (!self.captureVideoPreviewLayer) {
+		self.captureVideoPreviewLayer = [AVCaptureVideoPreviewLayer layerWithSession:self.captureSession];
+	}
+	
+	//if you want to adjust the previewlayer frame, here!
+	self.captureVideoPreviewLayer.frame = self.view.bounds;
+	self.captureVideoPreviewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+	[self.view.layer addSublayer: self.captureVideoPreviewLayer];
+	[self.captureSession startRunning];
+}
+/* End of test code */
 @end
 
 
