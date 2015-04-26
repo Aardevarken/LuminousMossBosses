ca6a8f3161c8c4e18324479d5420baebab2aa481
Jacob Rail <jacot.rail@colorado.edu>
2015-04-25 23:13:22 -0600

    added cell class for the filter

---
+++ iOS/DeveloperBuild/DeveloperBuild/FilterTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  FilterTableViewCell.h
+//  DeveloperBuild
+//
+//  Created by Jacob Rail on 4/25/15.
+//  Copyright (c) 2015 CU Boulder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FilterTableViewCell : UITableViewCell
+
+@end
