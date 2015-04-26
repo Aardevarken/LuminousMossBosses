7988117498da0f59134dc5a19904117a5e039f10
Jacob Rail <jacot.rail@colorado.edu>
2015-04-25 23:07:25 -0600

    starting to hook filter view controller up. this commit serves as a reset point

---
+++ iOS/DeveloperBuild/DeveloperBuild/FilterViewController.h
@@ -0,0 +1,19 @@
+//
+//  FilterViewController.h
+//  DeveloperBuild
+//
+//  Created by Jacob Rail on 4/25/15.
+//  Copyright (c) 2015 CU Boulder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FilterViewController : UIViewController// <UITableViewDelegate, UITableViewDataSource>{}
+// Buttons
+//- (IBAction)cancelButton:(UIButton *)sender;
+//- (IBAction)searchButton:(UIButton *)sender;
+
+// Table View
+//@property (strong, nonatomic) IBOutlet UITableView *filterTableView;
+
+@end
---
+++ iOS/DeveloperBuild/DeveloperBuild/FilterViewController.m
@@ -0,0 +1,46 @@
+//
+//  FilterViewController.m
+//  DeveloperBuild
+//
+//  Created by Jacob Rail on 4/25/15.
+//  Copyright (c) 2015 CU Boulder. All rights reserved.
+//
+
+#import "FilterViewController.h"
+
+@interface FilterViewController ()
+
+@end
+
+@implementation FilterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)cancelButton:(UIButton *)sender {
+	[[self navigationController] popViewControllerAnimated:YES];
+}
+
+- (IBAction)searchButton:(UIButton *)sender {
+	[[self navigationController] popViewControllerAnimated:YES];
+}
+
+@end
