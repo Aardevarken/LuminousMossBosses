585dc16dbb13fe5ff66e626b2fe35f3390d67c66
jamiefmiller <jamie.miller273@gmail.com>
2015-05-02 00:18:43 +0000

    making flower trainger fully directory independent

--- Flower/train_db.sh
+++ Flower/train_db.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-make manifest_db
-./manifest_db
-rm -f flower.vec
-opencv_createsamples -vec flower.vec -info positive.manifest -w 20 -h 20
-mkdir -p classifier
-rm -f classifier/*
-opencv_traincascade -data classifier -vec flower.vec -bg background.manifest -numPos 90 -numNeg 101 -numStages 25 -w 20 -h 20 -featureType HAAR
-cp classifier/cascade.xml flower.xml
+my_dir=`dirname $0`
+
+make $my_dir/manifest_db 
+$my_dir/manifest_db
+rm -f $my_dir/flower.vec
+opencv_createsamples -vec $my_dir/flower.vec -info $my_dir/positive.manifest -w 20 -h 20
+mkdir -p $my_dir/classifier
+rm -f $my_dir/classifier/*
+opencv_traincascade -data $my_dir/classifier -vec $my_dir/flower.vec -bg $my_dir/background.manifest -numPos 90 -numNeg 101 -numStages 25 -w 20 -h 20 -featureType HAAR
+cp $my_dir/classifier/cascade.xml $my_dir/flower.xml
