1bf18b6e3db465001c1a37a13eebd0a10f74f251
jamiefmiller <jamie.miller273@gmail.com>
2015-05-03 19:04:32 +0000

    Removing timestamps.

--- BagOfWords/genxml_db.sh
+++ BagOfWords/genxml_db.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 my_dir=`dirname $0`
-stamp=`date +'_%Y-%m-%d_%H:%M:%S'`
 
 rm -f vocabulary.xml
 make $my_dir/build_vocab_db
 $my_dir/build_vocab_db
-cp vocabulary.xml $1/vocabulary$stamp.xml
+cp vocabulary.xml $1/vocabulary.xml
 
 rm -f silene.xml
 make $my_dir/bow_train_db
 $my_dir/bow_train_db
-mv silene.xml $1/silene$stamp.xml
+mv silene.xml $1/silene.xml
 rm -f vocabulary.xml
 
