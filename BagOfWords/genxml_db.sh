52be27f455801e730deac83e0b1c1691e07648b6
Ubuntu <ubuntu@ip-172-31-41-141.us-west-2.compute.internal>
2015-05-04 05:06:33 +0000

    update makefile

--- BagOfWords/genxml_db.sh
+++ BagOfWords/genxml_db.sh
@@ -2,12 +2,10 @@
 my_dir=`dirname $0`
 
 rm -f vocabulary.xml
-make $my_dir/build_vocab_db
 $my_dir/build_vocab_db
 cp vocabulary.xml $1/vocabulary.xml
 
 rm -f silene.xml
-make $my_dir/bow_train_db
 $my_dir/bow_train_db
 mv silene.xml $1/silene.xml
 rm -f vocabulary.xml
