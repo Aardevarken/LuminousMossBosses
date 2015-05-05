6fe102f3040c1f18b530db333fcca43ffc67fc36
Jacob Rail <jacot.rail@colorado.edu>
2015-05-04 20:41:40 -0600

    fixed table rows in fieldguide DVC to use text views.

--- iOS/DeveloperBuild/DeveloperBuild/FieldGuideDetailViewController.m
+++ iOS/DeveloperBuild/DeveloperBuild/FieldGuideDetailViewController.m
@@ -56,16 +56,14 @@ static NSDictionary* typeMap = nil;
 	
 	NSArray *keys = [mdic allKeys];
 	for (int i = 0 ; i < [keys count]; i++){
-		if ([mdic[keys[i]] isEqualToString:@".."] || [mdic[keys[i]] isEqualToString:@""]){
+		if ([mdic[keys[i]] isEqualToString:@"."] || [mdic[keys[i]] isEqualToString:@""]){
 			[mdic removeObjectForKey:keys[i]];
 		}
 	}
 	
 	[mdic removeObjectForKey:@"code"];
 	[mdic removeObjectForKey:@"latin_name"];
-	//[mdic removeObjectForKey:@"description"];
-	
-	
+		
 	speciesInfo = mdic;
 	valueInfoToDisplay = [speciesInfo allValues];
 	keyInfoToDisplay = [speciesInfo allKeys];
@@ -91,40 +89,62 @@ CGFloat textViewFontSize = 16.0f;
 
 - (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
 	DetailFieldGuidCell *cell = [speciesInfoTableView dequeueReusableCellWithIdentifier:@"DetailFieldGuideCell_ID_2"];
-	
-	
 
 	NSString *val = [valueInfoToDisplay objectAtIndexedSubscript:indexPath.row];
 	NSString *t = [typeMap objectForKey:[keyInfoToDisplay objectAtIndex: indexPath.row]];
 	
-	cell.title.text = t;//[typeMap objectForKey:[keyInfoToDisplay objectAtIndex:indexPath.row]];
+	cell.title.text = t;
+//	
+//	NSString *randtext2 = @"Unpacked reserved sir offering bed judgment may and quitting speaking. Is do be improved raptures offering required in replying raillery. <END>"
+//	@"\n this is the next line"
+//	@"\n and the last line";
+//	BOOL debug = NO;
+//	NSString *tv;
+//	if ([t isEqualToString:@"Description"]) {
+//		tv = randtext2;
+//		debug = YES;
+//	}
+//	else {
+//		tv = val;
+//		cell.textview.text = tv;
+//	}
 	
-	NSString *randtext2 = @"Unpacked reserved sir offering bed judgment may and quitting speaking. Is do be improved raptures offering required in replying raillery. <END>";
-	
-	NSString *tv;
-	if ([t isEqualToString:@"Description"]) {
-		tv = randtext2;
-	}
-	else {
-		tv = val;
-	}
-		
-	cell.textview.text = tv;
-	[cell.textview setFont:[UIFont systemFontOfSize:16.0f]];
+	cell.textview.text = val;
+	cell.textview.font = [UIFont systemFontOfSize:textViewFontSize];
+	cell.textview.textContainerInset = UIEdgeInsetsMake(0, 0, 0, 0);
+	cell.textview.textColor = [UIColor colorWithRed:86/255.0f green:86/255.0f blue:79/255.0f alpha:1.0f];
 
+//	NSMutableAttributedString *atv = [[NSMutableAttributedString alloc] initWithString:tv];
+//	UIFont *attfont = [UIFont systemFontOfSize:textViewFontSize];
+//	[atv addAttribute:NSFontAttributeName value:attfont range:NSMakeRange(0, [atv length])];
+//	[[cell textview] setAttributedText:atv];
+//	
 	NSAttributedString * celltext = [[NSMutableAttributedString alloc] initWithString:cell.textview.text];
-
+	
+	
 	CGFloat a = cell.title.frame.size.height;//[self textViewHeightForAttributedText:cellLabel andWidth:100];
 	CGFloat b = [self textViewHeightForAttributedText:celltext andWidth:cell.textview.frame.size.width];
 	[speciesInfoTableView setRowHeight:(a + b)];
-	NSLog(@"-----------------------\n"
-		  @"(a)title: %@ len: %lu\n"
-		  @"(b)value: %@ len: %lu\n"
-		  @"height: %f(a) + %f(b)",
-		  cell.title.text,(unsigned long)[t length],
-		  tv, (unsigned long)[tv length],
-		  a, b);
-	
+//	if (debug) {
+//		NSLog(@"-----------------------\n"
+//			  @"(a)title: %@ len: %lu\n"
+//			  @"(b)value: %@ len: %lu\n"
+//			  @"height: %f(a) + %f(b)\n",
+//			  cell.title.text,(unsigned long)[t length],
+//			  tv, (unsigned long)[tv length],
+//			  a, b);
+//		
+//		NSLog(@"\n"
+//			  @"contentSize h:%f  w:%f \n"
+//			  @"frameHeight h:%f  w:%f \n"
+//			  @"\n",
+//			  cell.textview.contentSize.height, cell.textview.contentSize.width,
+//			  cell.textview.frame.size.height, cell.textview.frame.size.width);
+//		
+//		NSLog(@"description: %@\n\n", cell.textview.description);
+//		NSLog(@"description: %@", cell.title.description);
+//
+//	}
 	return cell;
 }
 
@@ -133,8 +153,7 @@ CGFloat textViewFontSize = 16.0f;
 - (CGFloat)textViewHeightForAttributedText: (NSAttributedString*)text andWidth: (CGFloat)width {
 	UITextView *calculationView = [[UITextView alloc] init];
 	[calculationView setAttributedText:text];
-	CGFloat fontSize = 16.0f;
-	[calculationView setFont:[UIFont systemFontOfSize:fontSize]];
+	[calculationView setFont:[UIFont systemFontOfSize:textViewFontSize]];
 	CGSize size = [calculationView sizeThatFits:CGSizeMake(width, FLT_MAX)];
 	return size.height;
 }
